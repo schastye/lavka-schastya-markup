e29abf1e4908d76c3a51c8f55a52fd54669966d4
Roman Ganin <realetive@gmail.com>
2016-03-20 21:36:49 +0300

    Move to TARS

---
+++ static/js/mainvsf616y9zfr.js
@@ -0,0 +1,16 @@
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+'use strict';
+'use strick';
+
+$(function () {
+  console.log('ready!');
+});
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+"use strict";
