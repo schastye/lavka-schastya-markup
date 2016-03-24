1d0dca079e6efd01ec6ec9a999741148de3ec45d
Roman Ganin <realetive@gmail.com>
2016-03-24 17:48:28 +0300

    Minor design fixes

--- static/js/mainz48dyqzolxr.js
+++ static/js/mainz48dyqzolxr.js
@@ -2,14 +2,14 @@
 "use strict";
 "use strict";
 "use strict";
+"use strict";
+"use strict";
 'use strict';
 'use strick';
 
 $(function () {
   console.log('ready!');
 });
-"use strict";
-"use strict";
 "use strict";
 "use strict";
 "use strict";
