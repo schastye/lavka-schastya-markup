66014a7ef57e3457ada0a11cc77819ea888fd838
Roman Ganin <realetive@yandex.ru>
2016-06-06 06:10:31 +0300

    Set box image by selected variant

--- markup/modules/box-variant/box-variant.js
+++ markup/modules/box-variant/box-variant.js
@@ -5,16 +5,10 @@
 (function () {
   let initElem = document.querySelectorAll( '.box-variant__item' );
   if ( initElem.length ) {
-    let slider = new Peppermint(document.getElementById('peppermint'), {
-      speed: 500,
-      touchSpeed: 500,
-      mouseDrag: true,
-      onSlideChange: function ( slide ) {
-        initElem[ slide ].querySelectorAll( '.box-variant__select' )[ 0 ]
-          .checked = true;
-      }
-    } );
+    let slider,
+      initSlide = 0;
     [].slice.call(initElem).forEach( ( el, item ) => {
+      initSlide = el.querySelectorAll( '.box-variant__select' )[ 0 ].checked ? item : initSlide;
       el.addEventListener( 'click', event => {
         event.preventDefault();
         if ( window.zenscroll ) {
@@ -26,6 +20,17 @@
         document.getElementById('price__value').innerText = select.value * inputQuantity;
         slider.slideTo( item );
       }, false);
+    } );
+
+    slider = new Peppermint(document.getElementById('peppermint'), {
+      speed: 500,
+      touchSpeed: 500,
+      mouseDrag: true,
+      startSlide: initSlide,
+      onSlideChange: function ( slide ) {
+        initElem[ slide ].querySelectorAll( '.box-variant__select' )[ 0 ]
+          .checked = true;
+      }
     } );
   }
 
