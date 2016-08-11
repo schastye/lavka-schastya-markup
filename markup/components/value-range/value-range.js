ba5f61eccbbe58fa7fe202589c4a420b70021a3b
Realetive <realetive@yandex.ru>
2016-08-11 07:54:19 +0300

    Update order page Temporary disable JS on range buttons

--- markup/components/value-range/value-range.js
+++ markup/components/value-range/value-range.js
@@ -2,35 +2,35 @@
 
 'use strict';
 
-(function () {
-  let initElem = document.querySelectorAll( '.value-range' );
-  if ( initElem.length ) {
-    [].slice.call( initElem ).forEach( valueRange => {
-      let input = valueRange.querySelectorAll( '.value-range__value' )[ 0 ];
-      [].slice.call( valueRange.querySelectorAll( '.value-range__button' ) )
-        .forEach( valueRangeValue => {
-          valueRangeValue.addEventListener( 'click', event => {
-            event.preventDefault();
-            switch ( event.currentTarget.value ) {
-              case 'cart/remove':
-                if ( input.value === input.getAttribute( 'min' ) ) {
-                  input.value = input.getAttribute( 'min' );
-                } else {
-                  input.value--;
-                }
-                break;
-              case 'cart/add':
-                if ( input.value === input.getAttribute( 'max' ) ) {
-                  input.value = input.getAttribute( 'max' );
-                } else {
-                  input.value++;
-                }
-                break;
-              default:
-                break;
-            }
-          } );
-        } );
-    } );
-  }
-} )();
+// (function () {
+//   let initElem = document.querySelectorAll( '.value-range' );
+//   if ( initElem.length ) {
+//     [].slice.call( initElem ).forEach( valueRange => {
+//       let input = valueRange.querySelectorAll( '.value-range__value' )[ 0 ];
+//       [].slice.call( valueRange.querySelectorAll( '.value-range__button' ) )
+//         .forEach( valueRangeValue => {
+//           valueRangeValue.addEventListener( 'click', event => {
+//             event.preventDefault();
+//             switch ( event.currentTarget.value ) {
+//               case 'cart/remove':
+//                 if ( input.value === input.getAttribute( 'min' ) ) {
+//                   input.value = input.getAttribute( 'min' );
+//                 } else {
+//                   input.value--;
+//                 }
+//                 break;
+//               case 'cart/add':
+//                 if ( input.value === input.getAttribute( 'max' ) ) {
+//                   input.value = input.getAttribute( 'max' );
+//                 } else {
+//                   input.value++;
+//                 }
+//                 break;
+//               default:
+//                 break;
+//             }
+//           } );
+//         } );
+//     } );
+//   }
+// } )();
