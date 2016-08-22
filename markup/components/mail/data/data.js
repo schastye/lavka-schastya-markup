c8ebc0556c846c60fa5b7d7e34d85b2005f86bc4
Realetive <realetive@yandex.ru>
2016-08-22 09:05:38 +0300

    Add mails templates

---
+++ markup/components/mail/data/data.js
@@ -0,0 +1,51 @@
+var data = {
+  mail: {
+    products: [
+      {
+        id: 1
+      , count: 1
+      , url: '#'
+      , name: 'Наборный товар'
+      , title: 'Наборный товар'
+      , price: 1000
+      , weight: 1000
+      , thumb: 'image.jpg'
+      , options: {
+          composition: '34,34,34,34,35,35,35,36'
+        , box: 14
+        }
+      }
+    , {
+        id: 2
+      , count: 1
+      , url: '#'
+      , name: 'Торт'
+      , title: 'Торт'
+      , price: 1000
+      , weight: 1000
+      , thumb: 'image.jpg'
+      , options: {
+          box: 45
+        }
+      }
+    , {
+        id: 3
+      , count: 1
+      , url: '#'
+      , name: 'Открытка'
+      , title: 'Открытка'
+      , price: 1000
+      , weight: 1000
+      , thumb: 'image.jpg'
+      , options: {
+          from: 'Отправитель'
+        , to: 'Получатель'
+        , message: 'Текст сообщения'
+        }
+      }
+    ]
+  , order: {
+      delivery: 450
+    }
+  }
+}
