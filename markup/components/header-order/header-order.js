6e1e6a4bff167d18fef2f8b4dfa1984f25098974
Realetive <realetive@yandex.ru>
2016-08-23 04:28:34 +0300

    Update minicart

---
+++ markup/components/header-order/header-order.js
@@ -0,0 +1,53 @@
+'use strict';
+
+function updateMiniCart( fromForm ) {
+  console.log('update cart!');
+  let xhr = new XMLHttpRequest();
+  let dataFromForm = new FormData( fromForm );
+  xhr.open('POST', 'https://lavkaschastya.com/api/getMiniCart');
+  xhr.send( dataFromForm );
+
+  xhr.onload = function () {
+    console.log( 'xhr loaded' );
+    console.log( xhr );
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        let miniCart = document.getElementById('header-order');
+        miniCart.innerHTML = xhr.response;
+        [].slice.call( miniCart.querySelectorAll( '.header-shopping-item form' ) ).forEach( function ( removeButton ) {
+          removeButton.addEventListener('submit', function ( event ) {
+            event.preventDefault();
+            updateMiniCart( this );
+          });
+        } );
+      }
+    }
+  };
+}
+
+(function () {
+  let formOrder = document.querySelector('form#order');
+  if ( formOrder ) {
+    formOrder.addEventListener( 'submit', function ( event ) {
+      let addToCart = document.querySelector( '.button_add-to-card' );
+      if ( addToCart.classList.contains( 'button_add-to-card_added' ) ) {
+        console.log( 'move' );
+      } else {
+        event.preventDefault();
+        updateMiniCart( this );
+        addToCart.classList.add( 'button_add-to-card_added' );
+        addToCart.querySelector( '.button__caption' ).textContent = 'Перейти к заказу';
+      }
+    }, false);
+
+  }
+  let removeFromMiniCart = document.querySelectorAll( '.header-shopping-item form' );
+  if ( removeFromMiniCart.length ) {
+    [].slice.call( removeFromMiniCart ).forEach( function ( removeButton ) {
+      removeButton.addEventListener( 'submit', function ( event ) {
+        event.preventDefault();
+        updateMiniCart( this );
+      }, false);
+    } );
+  }
+})();
