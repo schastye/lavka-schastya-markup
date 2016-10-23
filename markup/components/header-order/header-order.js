6e2ef9cdc9a7f9c956d37b034b17f0fe6775e87b
Realetive <realetive@yandex.ru>
2016-10-23 16:16:55 +0300

    Update miniCart

--- markup/components/header-order/header-order.js
+++ markup/components/header-order/header-order.js
@@ -1,23 +1,34 @@
 'use strict';
 
-function updateMiniCart( fromForm ) {
-  console.log('update cart!');
+function updateMiniCart( fromForm, action ) {
+  console.log( 'update…' );
   let xhr = new XMLHttpRequest();
   let dataFromForm = new FormData( fromForm );
-  xhr.open('POST', 'https://lavkaschastya.com/api/getMiniCart');
+  switch ( action ) {
+    case 'add':
+      dataFromForm.append( 'ms2_action', 'cart/add' );
+      break;
+    case 'remove':
+      dataFromForm.append( 'ms2_action', 'cart/remove' );
+      break;
+    default:
+      dataFromForm.append( 'ms2_action', 'cart/update' );
+      break;
+  }
+  xhr.open('POST', '//lavkaschastya.com/api/getMiniCart');
   xhr.send( dataFromForm );
-
+  console.log( 'send' );
   xhr.onload = function () {
-    console.log( 'xhr loaded' );
-    console.log( xhr );
     if (xhr.readyState === xhr.DONE) {
       if (xhr.status === 200) {
+        console.log( 'respond' );
+        console.log( 'xhr.response' );
         let miniCart = document.getElementById('header-order');
         miniCart.innerHTML = xhr.response;
         [].slice.call( miniCart.querySelectorAll( '.header-shopping-item form' ) ).forEach( function ( removeButton ) {
           removeButton.addEventListener('submit', function ( event ) {
             event.preventDefault();
-            updateMiniCart( this );
+            updateMiniCart( this, 'remove' );
           });
         } );
       }
@@ -26,28 +37,31 @@ function updateMiniCart( fromForm ) {
 }
 
 (function () {
+  console.log( 'enter' );
   let formOrder = document.querySelector('form#order');
   if ( formOrder ) {
+    console.log( 'if formOrder' );
     formOrder.addEventListener( 'submit', function ( event ) {
+      console.log( 'event' );
+      event.preventDefault();
       let addToCart = document.querySelector( '.button_add-to-card' );
       if ( addToCart.classList.contains( 'button_add-to-card_added' ) ) {
-        event.preventDefault();
-        window.location.href = 'https://lavkaschastya.com/order';
+        window.location.href = '//lavkaschastya.com/order';
       } else {
-        event.preventDefault();
-        updateMiniCart( this );
+        console.log( 'ready to update' );
+        updateMiniCart( this, 'add' );
         addToCart.classList.add( 'button_add-to-card_added' );
         addToCart.querySelector( '.button__caption' ).textContent = 'Перейти к заказу';
       }
     }, false);
-
   }
+
   let removeFromMiniCart = document.querySelectorAll( '.header-shopping-item form' );
   if ( removeFromMiniCart.length ) {
     [].slice.call( removeFromMiniCart ).forEach( function ( removeButton ) {
       removeButton.addEventListener( 'submit', function ( event ) {
         event.preventDefault();
-        updateMiniCart( this );
+        updateMiniCart( this, 'remove' );
       }, false);
     } );
   }
